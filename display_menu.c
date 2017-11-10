a7f1f88241648e0df43729e6200c3f2938f274f1
wansik2 <wsdosaa@naver.com>
2017-11-10 16:17:09 +0900

    display_menu modify

--- display_menu.c
+++ display_menu.c
@@ -6,13 +6,17 @@ int display_menu(void)
 	{
 		system("clear");
 		printf("\n\n\t\t\t\tText Tetris");
-		printf("\n\t\t\t========================");
+		printf("\n\t\t\t===========================");
 		printf("\n\t\t\t\GAME MENU\t\n");
-		printf("\n\t\t\t========================");
-		printf("\n\t\t\t=\t1) GAME START\t   =");
-		printf("\n\t")
+		printf("\n\t\t\t===========================");
+		printf("\n\t\t\t=\t1) GAME START\t      =");
+		printf("\n\t\t\t=\t2) SEARCH RECORD\t   =");
+		printf("\n\t\t\t=\t3) PRINT  RECORD\t   =");
+		printf("\n\t\t\t=\t4) EXIT GAME\t\t     =");
+		printf("\n\t\t\t===========================");
+		printf("\n\t\t\t\t\t SELECT : ");
+		scanf("%d",&menu);
+
 	}
-
-
 }
 
