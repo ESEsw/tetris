0a19825e2a9f9e9f8060984de34c4ab95cacd884
wansik2 <wsdosaa@naver.com>
2017-11-10 16:11:15 +0900

    display_mebu

---
+++ display_menu.c
@@ -0,0 +1,18 @@
+int display_menu(void)
+{
+	int menu = 0;
+	
+	while(1)
+	{
+		system("clear");
+		printf("\n\n\t\t\t\tText Tetris");
+		printf("\n\t\t\t========================");
+		printf("\n\t\t\t\GAME MENU\t\n");
+		printf("\n\t\t\t========================");
+		printf("\n\t\t\t=\t1) GAME START\t   =");
+		printf("\n\t")
+	}
+
+
+}
+
