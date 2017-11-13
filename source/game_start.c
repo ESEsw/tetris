24427524a70d0675f9d1613d1bdad7b774e797c7
wansik2 <wsdosaa@naver.com>
2017-11-13 17:08:41 +0900

    game_start

---
+++ source/game_start.c
@@ -0,0 +1,18 @@
+// GAME START function
+
+int game_start(void)
+{
+	static struct sigaction sa;
+	static struct itimerval timer;
+	time_t ptime;
+	struct tm *t;
+	FILE *fp = NULL;
+
+	if(game == GAME_START)
+	{
+		
+	}
+
+
+
+}
