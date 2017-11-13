6663717d1dce445cbc92331188bc3e6219fc3a1a
wansik2 <wsdosaa@naver.com>
2017-11-13 17:19:55 +0900

    game_start.c

--- source/game_start.c
+++ source/game_start.c
@@ -24,38 +24,6 @@ int game_start(void)
 		timer.it_interval.tv_sec = 0;
 		timer.it_interval.tv_usec = 1;
 
-		
-		nclude <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-
-int main(int argc, char *argv[])
-{
-        int fd;
-        if (argc != 2)
-        {
-                fprintf(stderr,"usage: opentest filename\n");
-                exit(0);
-        }
-        //put your code
-        fd = open(argv[1],O_RDONLY);
-
-        if( fd == -1) // open failure
-        {
-                perror("error!\n");
-                exit(0);
-        }
-        else //open success
-        {
-                printf("open %s successful\n",argv[1]);
-        }
-
-        return 0;
-
 
 	}
 
