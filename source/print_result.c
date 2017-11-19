94827ea9f14fe69cadd30d7ab55e52455e42bb49
wansik2 <wsdosaa@naver.com>
2017-11-19 14:26:57 +0900

    print_result.c 1st modify

--- source/print_result.c
+++ source/print_result.c
@@ -1,15 +1,15 @@
- int print_result(void)
-766 {
-767     FILE *fp = NULL;
-768     char ch = 1 ;
-769 
-770     fp = fopen("result", "rb");
-771 
-772     if(fp == NULL)
-773         return 0;
-774 
-775     system("clear");
-776 
-777     printf("\n\t\t\t\tText Tetris");
-778     printf("\n\t\t\t\t 게임 기록\n\n");
-779     printf("\n\t\t이름\t\t점수\t 날짜\t\t 시간"); 
+int print_result(void)
+{
+     FILE *fp = NULL;
+     char ch = 1 ;
+
+     fp = fopen("result", "rb");
+
+     if(fp == NULL)
+         return 0;
+
+     system("clear");
+
+     printf("\n\t\t\t\tText Tetris");
+     printf("\n\t\t\t\t Game record\n\n");
+     printf("\n\t\t Name\t\t record\t date\t\t time"); 
