234d908ca05d9a2490a2b56266ab0176d9d6cbd8
wansik2 <wsdosaa@naver.com>
2017-11-19 14:27:52 +0900

    add the while

--- source/print_result.c
+++ source/print_result.c
@@ -13,3 +13,16 @@ int print_result(void)
      printf("\n\t\t\t\tText Tetris");
      printf("\n\t\t\t\t Game record\n\n");
      printf("\n\t\t Name\t\t record\t date\t\t time"); 
+while(1)
+782     {
+783             fread(&temp_result, sizeof(struct result), 1, fp);
+784             if(!feof(fp))
+785             {
+786                 printf("\n\t========================================================");
+787                 printf("\n\t\t%s\n\t\t\t\t%ld\t %d. %d. %d. | %d : %d\n", temp_result.name, temp_result.point, temp_result.year, temp_result.month, temp_result.day, temp_result.hour, temp_result.min);
+788             }
+789             else
+790             {
+791                 break;
+792             }
+793     } 
