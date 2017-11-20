9d3c5246bb50ecde4c504f2afea749ee0e3f4b36
Sung-jae <ksj66543@gmail.com>
2017-11-20 20:49:41 +0900

    make tetris.c

--- tetris.c
+++ tetris.c
@@ -87,3 +87,37 @@ char o_block[4][4][4] =
             1, 1, 0, 0,    1, 1, 0, 0,    0, 0, 0, 0,    0, 0, 0, 0,
     };
 
+char tetris_table[21][10];
+
+static struct result
+{
+    char name[30];
+    long point;
+    int year;
+    int month;
+    int day;
+    int hour;
+    int min;
+    int rank;
+}temp_result;
+
+int block_number = 0;
+int next_block_number = 0;
+int block_state = 0;
+int x = 3, y = 0;
+int game = GAME_END;
+int best_point = 0;
+long point = 0;
+
+int display_menu(void);
+int init_tetris_table(void);
+int display_tetris_table(void);
+int game_start(void);
+int refresh(int);
+int move_block(int);
+int drop(void);
+int collision_test(int);
+int check_one_line(void);
+int print_result(void);
+int search_result(void);
+int getch(void);
