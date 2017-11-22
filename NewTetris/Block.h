b8b1821458606a6565e50a52eca3a678e611912e
wansik2 <wsdosaa@naver.com>
2017-11-22 20:03:47 +0900

    Block add

---
+++ NewTetris/Block.h
@@ -0,0 +1,20 @@
+#define BLOCK_H
+
+struct Point{
+	int x,y;
+}
+
+class Block{
+	private:
+	static const int LEFT,RIGHT,BOTTOM;
+	int stat;
+	Point prev;
+	void init(int type);
+	public:
+	static const char * BLOCK;
+	static const char * CLEAR;
+	static Point zero;
+	
+
+
+}
