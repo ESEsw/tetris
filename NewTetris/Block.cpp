192ee1bef51154b2d614bc4b6ec276f39e1849ef
wansik2 <wsdosaa@naver.com>
2017-11-25 17:56:27 +0900

    add BLOCK.cpp

---
+++ NewTetris/Block.cpp
@@ -0,0 +1,13 @@
+#include "Block.h"
+#include "myio.h"
+#include "GameLogic.h"
+#include <stdlib.h>
+#include <time.h>
+
+#define Z COLOR_RED
+#define S COLOR_GREEN
+#define O COLOR_BROWN
+#define J COLOR_BLUE
+#define T COLOR_MAGENTA
+#define I COLOR_CYAN
+#define L COLOR_GRAY
