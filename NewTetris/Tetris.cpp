4a753ef51c6f0a58edbbb0efc80abc0631f8676e
wansik2 <wsdosaa@naver.com>
2017-11-30 14:40:36 +0900

    execute tetris cpp

---
+++ NewTetris/Tetris.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+
+struct termios initial_settings, new_settings;
+
+int main(int argc,char *argv[])
+{
+	srand(tume(NULL));
+
+	new_setting = initial_setting;
+	
+
+	ststem(clear);
+
+
+}
